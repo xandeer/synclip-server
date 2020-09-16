28c924ca043d4e6899d405842229f81110db1771
Kevin Du <kkxandeer@gmail.com>
2020-09-16 20:49:09 +0800

    feat: add logger middle ware

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,12 +1,19 @@
-import { Module } from '@nestjs/common';
+import { Module, NestModule, MiddlewareConsumer } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { ClipboardService } from './clipboard/clipboard.service';
 import { ClipboardController } from './clipboard/clipboard.controller';
+import { LoggerMiddleware } from './middleware/logger.middleware';
 
 @Module({
   imports: [],
   controllers: [AppController,  ClipboardController],
   providers: [AppService,  ClipboardService],
 })
-export class AppModule {}
+export class AppModule implements NestModule {
+  configure(consumer: MiddlewareConsumer) {
+    consumer
+      .apply(LoggerMiddleware)
+      .forRoutes(ClipboardController);
+  }
+}
